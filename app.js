d67748ed828b65b9ce5759500626c35860736e7d
Le_chinoix <nicolas.ngomai@gmail.com>
2016-08-05 00:34:49 +0200

    Fix local functioning

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var
   request = require('request');
 
 var app = express();
+app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
